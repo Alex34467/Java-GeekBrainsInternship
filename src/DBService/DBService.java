3cf3547b855619dce5b9f5703f3a41996950fee7
Alex34467 <borisov.alexey0@gmail.com>
2017-08-13 19:45:55 +0300

    Рефакторинг метода getSites.

--- src/DBService/DBService.java
+++ src/DBService/DBService.java
@@ -1,25 +1,18 @@
 package DBService;
 
-import DBService.Repository.KeywordRepository;
-import DBService.Repository.PersonRepository;
-import Entities.Keyword;
-import Entities.Page;
-import Entities.Person;
-import Entities.Site;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import DBService.Repository.*;
+import Entities.*;
+import java.util.*;
 
 // БД сервис.
 public class DBService
 {
     // Данные.
     private static DBService instance;
-    private DBExecutor executor;
     private PersonRepository personRepository;
     private KeywordRepository keywordRepository;
+    private SiteRepository siteRepository;
+    private PageRepository pageRepository;
 
 
     // Геттер.
@@ -32,9 +25,6 @@ public class DBService
     // Возврат списка личностей.
     public Collection<Person> getPersons()
     {
-        // Данные.
-        String query;
-
         // Получение личностей.
         Collection<Person> persons = personRepository.getAll();
 
@@ -42,7 +32,7 @@ public class DBService
         for (Person person : persons)
         {
             Collection<Keyword> keywords = keywordRepository.getAllKeywordsByPersonId(person.getId());
-            person.setKeywords(keywords);
+            person.addKeywords(keywords);
         }
 
         // Возврат результата.
@@ -50,64 +40,32 @@ public class DBService
     }
 
     // Возврат списка страниц.
-    public Set<Site> getSites()
+    public Collection<Site> getSites()
     {
-        try
+        // Получение сайтов.
+        Collection<Site> sites = siteRepository.getAll();
+
+        // Получение страниц.
+        for (Site site : sites)
         {
-            // Данные.
-            String query;
-            Set<Site> sites = new HashSet<>();
-
-            // Получение списка сайтов.
-            System.out.println("Looking for sites.");
-            query = "SELECT * FROM Sites";
-            ResultSet resultSet = executor.executeQuery(query);
-
-            // Обход результата.
-            while (resultSet.next())
-            {
-                Site site = new Site(resultSet.getInt(1), resultSet.getString(2));
-                sites.add(site);
-                System.out.println("   Found site: " + site);
-            }
-            System.out.println("SiteRepository search completed.\n");
-
-            // Обход списка сайтов.
-            for (Site site : sites)
-            {
-                // Подготовка.
-                System.out.println("Looking for pages for site: " + site.getName());
-                query = "SELECT * FROM pages WHERE SiteID = " + site.getId();
-                ResultSet resultSet2 = executor.executeQuery(query);
-
-                // Подучение страниц.
-                while (resultSet2.next())
-                {
-                    Page page = new Page(resultSet2.getInt(1), resultSet2.getString(2), resultSet2.getInt(3), resultSet2.getString(4), resultSet2.getString(5));
-                    site.addPage(page);
-                    System.out.println("   Found page: " + page);
-                }
-                System.out.println("Pages search completed.\n");
-            }
-
-            // Возврат результата.
-            return sites;
+            Collection<Page> pages = pageRepository.getAllPagesBySiteId(site.getId());
+            site.addPages(pages);
         }
-        catch (SQLException e)
-        {
-            e.printStackTrace();
-            return null;
-        }
+
+        // Возврат результата.
+        return sites;
     }
 
     // Конструктор.
     private DBService()
     {
         // Инициализация исполнителя.
-        executor = MySQLExecutor.getInstance();
+        DBExecutor executor = MySQLExecutor.getInstance();
 
         // Инициализация репозиториев.
         personRepository = new PersonRepository(executor);
         keywordRepository = new KeywordRepository(executor);
+        siteRepository = new SiteRepository(executor);
+        pageRepository = new PageRepository(executor);
     }
 }
