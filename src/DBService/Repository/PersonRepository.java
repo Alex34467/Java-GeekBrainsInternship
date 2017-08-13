3637a042f6478e582323e117c50dd32a98b1b2b3
Alex34467 <borisov.alexey0@gmail.com>
2017-08-13 18:19:24 +0300

    Дополнение PersonRepository.

--- src/DBService/Repository/PersonRepository.java
+++ src/DBService/Repository/PersonRepository.java
@@ -1,6 +1,7 @@
 package DBService.Repository;
 
 import Entities.Person;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 // Репизиторий личности.
 public class PersonRepository implements Repository<Person>
@@ -9,27 +10,27 @@ public class PersonRepository implements Repository<Person>
     @Override
     public void add(Person person)
     {
-
+        throw new NotImplementedException();
     }
 
     // Выбор.
     @Override
     public Person getById(int id)
     {
-        return null;
+        throw new NotImplementedException();
     }
 
     // Удаление.
     @Override
     public void delete(Person person)
     {
-
+        throw new NotImplementedException();
     }
 
     // Обновление.
     @Override
     public void update(Person person)
     {
-
+        throw new NotImplementedException();
     }
 }
