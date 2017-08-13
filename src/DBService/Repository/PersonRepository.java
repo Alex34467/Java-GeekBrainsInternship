9c520dcad4f29d27eb8fbbb729682d90f5845880
Alex34467 <borisov.alexey0@gmail.com>
2017-08-13 19:15:47 +0300

    Добавление ссылки на исполнителя. Добавление конструктора. Реализация метода getAll.

--- src/DBService/Repository/PersonRepository.java
+++ src/DBService/Repository/PersonRepository.java
@@ -1,13 +1,27 @@
 package DBService.Repository;
 
 import Entities.Person;
+import DBService.*;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collection;
+import java.util.HashSet;
 
 
 // Репизиторий личности.
 public class PersonRepository implements Repository<Person>
 {
+    // Данные.
+    private DBExecutor executor;
+
+
+    // Конструктор.
+    public PersonRepository(DBExecutor executor)
+    {
+        this.executor = executor;
+    }
+
     // Добавление.
     @Override
     public void add(Person person)
@@ -26,7 +40,27 @@ public class PersonRepository implements Repository<Person>
     @Override
     public Collection<Person> getAll()
     {
-        throw new NotImplementedException();
+        // Подготовка запроса.
+        String query = "SELECT * FROM Persons";
+        ResultSet resultSet = executor.executeQuery(query);
+
+        // Обработка результата.
+        Collection<Person> persons = new HashSet<>();
+        try
+        {
+            while (resultSet.next())
+            {
+                Person person = new Person(resultSet.getInt(1), resultSet.getString(2));
+                persons.add(person);
+            }
+        }
+        catch (SQLException e)
+        {
+            return persons;
+        }
+
+        // Возврат результата.
+        return persons;
     }
 
     // Удаление.
