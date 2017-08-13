8ba94e68a198659865fe4136b6fdd13907fbe098
Alex34467 <borisov.alexey0@gmail.com>
2017-08-13 18:18:42 +0300

    Создание KeywordRepository.

---
+++ src/DBService/Repository/KeywordRepository.java
@@ -0,0 +1,44 @@
+package DBService.Repository;
+
+import Entities.Keyword;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.Set;
+
+// Репозиторий ключевых слов.
+public class KeywordRepository implements Repository<Keyword>
+{
+    // Добавление.
+    @Override
+    public void add(Keyword keyword)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Выбор.
+    @Override
+    public Keyword getById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Выбор ключевых слов по Id личности.
+    public Set<Keyword> getKeywordsByPersonId(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Удаление.
+    @Override
+    public void delete(Keyword keyword)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Обновление.
+    @Override
+    public void update(Keyword keyword)
+    {
+        throw new NotImplementedException();
+    }
+}
