0aeea48e6e523e90298a4c2a9b9af400f48a4da4
Alex34467 <borisov.alexey0@gmail.com>
2017-08-15 17:03:21 +0300

    Добавлен класс KeywordParser.

---
+++ src/Parsing/KeywordParser.java
@@ -0,0 +1,12 @@
+package Parsing;
+
+// Парсер ключевых слов.
+public class KeywordParser
+{
+    // Парсинг.
+    public static int countMatches(String url, String keyword)
+    {
+        // Возврат значения.
+        return 0;
+    }
+}
