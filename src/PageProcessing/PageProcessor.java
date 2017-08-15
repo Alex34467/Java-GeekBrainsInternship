cab728fc267b2e72650570be82e4b167b96dc30c
Alex34467 <borisov.alexey0@gmail.com>
2017-08-15 17:04:57 +0300

    Реализован метод processPage.

--- src/PageProcessing/PageProcessor.java
+++ src/PageProcessing/PageProcessor.java
@@ -1,11 +1,16 @@
 package PageProcessing;
 
 import DBService.DBService;
+import Entities.Keyword;
 import Entities.Page;
+import Entities.Person;
+import Entities.PersonsPageRank;
 import PageProcessing.XMLParser.XMLParser;
+import Parsing.KeywordParser;
 import Parsing.RobotsParser;
 import Util.Util;
 
+import java.util.Collection;
 import java.util.Set;
 
 // Обработчик страниц.
@@ -63,6 +68,27 @@ public class PageProcessor
     // Обработка страницы.
     public void processPage(Page page)
     {
+        // Получение списка личностей.
         System.out.println("   Парсинг текста.");
+        Collection<Person> persons = DBService.getInstance().getPersons();
+
+        // Обход личностей.
+        for (Person person : persons)
+        {
+            // Получение ключевых слов.
+            Collection<Keyword> keywords = DBService.getInstance().getKeywordsByPersonId(person.getId());
+
+            // Обход ключевых слов.
+            int matches = 0;
+            for (Keyword keyword : keywords)
+            {
+                // Подсчет совпадеий.
+                matches += KeywordParser.countMatches(page.getUrl(), keyword.getName());
+            }
+
+            // Запись результата в БД.
+            PersonsPageRank rank = new PersonsPageRank(person.getId(), page.getId(), matches);
+            DBService.getInstance().addPersonsPageRank(rank);
+        }
     }
 }
